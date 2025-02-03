e0d0da5b79cb186d855616c1ae18f0fd389f7e9f
chemist-god <godtenkorang72@gmail.com>
2025-02-03 23:01:15 +0000

    modified compile js

--- compile.js
+++ compile.js
@@ -7,4 +7,4 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 // Compile the contract
-console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
