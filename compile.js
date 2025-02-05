45bf3e35b28e913c596da7e68d8a9439b76045f9
chemist-god <godtenkorang72@gmail.com>
2025-02-05 23:35:43 +0000

    `Update compile.js to use solc compiler API and output ABI and bytecode`

--- compile.js
+++ compile.js
@@ -6,5 +6,28 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
+// Prepare the input for the compiler
+const input = {
+    language: 'Solidity',
+    sources: {
+        'Inbox.sol': {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*'], // Select all output
+            },
+        },
+    },
+};
+
 // Compile the contract
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+// Export the interface and bytecode
+module.exports = {
+    interface: output.contracts['Inbox.sol'].Inbox.abi,
+    bytecode: output.contracts['Inbox.sol'].Inbox.evm.bytecode.object,
+};
