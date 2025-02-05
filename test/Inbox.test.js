3b3ca384047130c4ec506987af6e5a86bc6d029c
chemist-god <godtenkorang72@gmail.com>
2025-02-05 23:51:05 +0000

    add assertion in Inbox.test.js`

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -20,7 +20,6 @@ beforeEach(async () => {
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        //assert.ok(inbox.options.address); // Check if the contract has an address
-    console.log(inbox);
+        assert.ok(inbox.options.address); // Check if the contract has an address
     });
 });
